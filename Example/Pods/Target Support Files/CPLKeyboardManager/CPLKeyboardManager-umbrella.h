ed27a383d1d4845084249e9e32d7c1f505cfbdce
Michał Ziętera <Michal.Zietera@cybercom.com>
2017-04-12 13:55:07 +0200

    Restructured project for proper unit testing purpose

--- Example/Pods/Target Support Files/CPLKeyboardManager/CPLKeyboardManager-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double CPLKeyboardManagerVersionNumber;
-FOUNDATION_EXPORT const unsigned char CPLKeyboardManagerVersionString[];
-
--- Example/Pods/Target Support Files/Pods-CPLKeyboardManager_Tests/Pods-CPLKeyboardManager_Tests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_CPLKeyboardManager_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CPLKeyboardManager_TestsVersionString[];
-
